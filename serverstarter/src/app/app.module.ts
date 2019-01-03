0609dea1f15ee92521190449fffef06d2b51bede
Max Carlquist <tenkaklet@gmail.com>
2019-01-03 14:30:16 +0100

    testar igen, tömt chachen

---
+++ serverstarter/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms' 
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component'; // utan filändelse!
+
+
+
+@NgModule({ // unik startup
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  // här kopplas bs in
+  bootstrap: [AppComponent] 
+})
+export class AppModule { }
+// export class ServerComponent {}
