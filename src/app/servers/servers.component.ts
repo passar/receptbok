0835e82ea4ec0ce8c458e1d395726133bb9aa478
Max Carlquist <tenkaklet@gmail.com>
2019-01-03 11:21:35 +0100

    lagt till att lägga till servrarna till att vara en array

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -17,6 +17,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!'
   serverName = "Testserver"
   serverCreated = false
+  servers = ['Testserver', 'Testserver 2']
 
   constructor() {
     setTimeout(() => {
@@ -29,6 +30,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true
+    this.servers.push(this.serverName)
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
   }
 
