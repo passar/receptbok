36a87b7be6d280a03158c83ee358310f26c1e627
Andreas Sandskär <ekrarna@gmail.com>
2019-01-03 16:00:49 +0100

    testpublicering recept

--- shopbootstrap/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopbootstrap/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe('A Recipe', 'Simply a test', 'https://images.unsplash.com/photo-1525385133512-2f3bdd039054?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=632&q=80')
+  ]
 
   constructor() { }
 
